db27ce1072d93749b81a2143563416dfd93cae51
laaouatni <laaouatni.anas@outlook.it>
2022-02-13 19:24:22 +0100

    eliminato bug nav

--- docs/css/pages/navbar/navbar.js
+++ docs/css/pages/navbar/navbar.js
@@ -4,21 +4,21 @@ let navLinks = document.querySelectorAll('.nav-text-container>span');
 navIcon.forEach((item, index, arr) => {
     item.addEventListener('mouseover', (e) => {
         item.classList.add('hovered');
-        navLinks[index - 1].classList.add('hovered');
+        navLinks[index].classList.add('hovered');
     });
     item.addEventListener('mouseout', (e) => {
         item.classList.remove('hovered');
-        navLinks[index - 1].classList.remove('hovered');
+        navLinks[index].classList.remove('hovered');
     });
 });
 
 navLinks.forEach((item, index, arr) => {
     item.addEventListener('mouseover', (e) => {
         item.classList.add('hovered');
-        navIcon[index + 1].classList.add('hovered');
+        navIcon[index].classList.add('hovered');
     });
     item.addEventListener('mouseout', (e) => {
         item.classList.remove('hovered');
-        navIcon[index + 1].classList.remove('hovered');
+        navIcon[index].classList.remove('hovered');
     });
 });
